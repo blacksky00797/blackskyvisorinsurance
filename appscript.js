4a8b2d02add783c8f34a0c7dc3528fdd57d8cb31
Thant Zin Aung <58395584+blacksky00797@users.noreply.github.com>
2022-12-08 22:25:30 +0630

    Add files via upload

--- appscript.js
+++ appscript.js
@@ -1,11 +1,25 @@
 let updateSection = document.querySelector("#update-contact");
-let updateButton = document.querySelector(".user-info-wrapper .update-button");
+let updateContactSection = document.querySelector("#update-contact .update-contact-wrapper");
+let successUpdateSection = document.querySelector("#update-contact .success-update-wrapper");
+let updateContactButton = document.querySelector(".user-info-wrapper .update-button");
+let updateButton = document.querySelector("#update-contact .update-contact-wrapper .right-items-wrapper .button-wrapper .update-button")
 let cancelButton = document.querySelector("#update-contact .update-contact-wrapper .right-items-wrapper .button-wrapper .cancel-button");
+let okButton = document.querySelector("#update-contact .success-update-wrapper .ok-button-wrapper");
 
-updateButton.addEventListener("click",e => {
+updateContactButton.addEventListener("click",e => {
     updateSection.setAttribute("style","display: flex");
 });
 
 cancelButton.addEventListener("click", e => {
     updateSection.setAttribute("style","display: none");
+});
+
+updateButton.addEventListener("click",e => {
+    updateContactSection.setAttribute("style","display: none");
+    successUpdateSection.setAttribute("style","display: flex");
+});
+
+okButton.addEventListener("click",e => {
+    updateSection.setAttribute("style","display: none");
+    successUpdateSection.setAttribute("style","display: none");
 });
